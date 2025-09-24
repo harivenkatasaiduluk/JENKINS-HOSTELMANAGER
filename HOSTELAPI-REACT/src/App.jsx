5c30b7148f4de0e7f90c32eef747de57c1394aff
KHVS <2300032396@kluniversity.in>
2025-09-25 00:25:29 +0530

    v1.0

--- HOSTELAPI-REACT/src/App.jsx
+++ HOSTELAPI-REACT/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import AddHostel from "./components/AddHostel";
 import ViewHostels from "./components/ViewHostels";
@@ -7,14 +7,14 @@ import FindHostel from "./components/FindHostel";
 
 const App = () => {
   return (
-    <Router basename="/reacthostelapi">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/add" element={<AddHostel />} />
         <Route path="/view" element={<ViewHostels />} />
         <Route path="/find" element={<FindHostel />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
